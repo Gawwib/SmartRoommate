4ce8f1de680fe0bc757cf580ff9b8e4b87262eef
Gawwib <amar.garibovic@stu.ibu.edu.ba>
2026-01-22 16:02:24 +0100

    Fix vercel

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const pool = mysql.createPool({
+const poolConfig = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
   user: process.env.DB_USER,
@@ -11,6 +11,12 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
-});
+};
+
+if (process.env.DB_SSL === 'true') {
+  poolConfig.ssl = { rejectUnauthorized: false };
+}
+
+const pool = mysql.createPool(poolConfig);
 
 module.exports = pool;
